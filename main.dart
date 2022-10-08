d7f3b3ccc1c7b3043af1daf1f7ce3e51514fb878
Ekkim-a <73611637+Ekkim-a@users.noreply.github.com>
2022-10-08 22:27:38 +0700

    Part about variables and constant

--- main.dart
+++ main.dart
@@ -1,3 +1,30 @@
 void main(){
-  print("Hello, METANIT.COM!");
+  //using variables
+  String name = "Tom";
+  print(name);
+  name = "Bob";
+  print(name);
+  //with var
+  var nameVar = "Varya";
+  print(nameVar);
+  //but cant change type
+  //nameVar = 45; - error
+
+  //but can with dynamic
+  dynamic nameDynamic = "Dimon";
+  print(nameDynamic);
+  nameDynamic = 45;
+  print(nameDynamic);
+
+  //const and final
+  const nameConst = "Kostya";//it's installizing in compilation
+  print(nameConst);
+  final nameFinal = "Fenya";//it's installizing in runtime
+  print(nameFinal);
+  //type of installizing constant and interpolation
+  const a = 5;
+  const int b = 6;
+  final int c = 8;
+  final d = 9;
+  print("a:$a b:$b c:$c d:$d");
 }
